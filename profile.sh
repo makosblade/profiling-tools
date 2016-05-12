bd92ace16a56835c92a0a88d4a61ee96da7b0972
Drew <makosblade@gmail.com>
2016-05-12 15:11:35 -0700

    allow multi-pid or single-pid

--- profile.sh
+++ profile.sh
@@ -1,4 +1,5 @@
-PID=$1
+TYPE=$1
+PID=$2
 DIR=`pwd`
 git submodule init
 git submodule update
@@ -14,6 +15,11 @@ fi
 cd out
 java -cp attach-main.jar:$JAVA_HOME/lib/tools.jar net.virtualvoid.perf.AttachOnce $PID
 cd $DIR
-perf record -F 99 -p $PID -g -- sleep 30; ./drewmaps $PID
+if [[ "$TYPE" -eq "multi" ]]
+then
+  perf record -F 99 -a -g -- sleep 30; ./Misc/java/jmaps
+else
+  perf record -F 99 -p $PID -g -- sleep 30; ./drewmaps $PID
+fi
 perf script > out.stacks01
 cat out.stacks01 | ./FlameGraph/stackcollapse-perf.pl | ./FlameGraph/flamegraph.pl --color=java --hash > flame01.svg
