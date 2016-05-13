2f883092c03e71cb801267966fc8f844b20d2592
Drew <makosblade@gmail.com>
2016-05-12 17:44:42 -0700

    cleanup

--- profile.sh
+++ profile.sh
@@ -1,5 +1,3 @@
-TYPE=$1
-PID=$2
 DIR=`pwd`
 git submodule init
 git submodule update
@@ -12,14 +10,18 @@ then
   ./bin/create-links-in ../
 fi
 
-cd out
-java -cp attach-main.jar:$JAVA_HOME/lib/tools.jar net.virtualvoid.perf.AttachOnce $PID
 cd $DIR
-if [[ "$TYPE" -eq "multi" ]]
+if [ $# -ge 1 ] 
 then
   perf record -F 99 -a -g -- sleep 30; ./Misc/java/jmaps
+  perf script > out.stacks01
+  cat out.stacks01 | ./FlameGraph/stackcollapse-perf.pl | ./FlameGraph/flamegraph.pl --color=java --hash > flame-all.svg
 else
   perf record -F 99 -p $PID -g -- sleep 30; ./drewmaps $PID
+  perf script > out.stacks01
+  cat out.stacks01 | ./FlameGraph/stackcollapse-perf.pl | ./FlameGraph/flamegraph.pl --color=java --hash > flame-$PID.svg
 fi
-perf script > out.stacks01
-cat out.stacks01 | ./FlameGraph/stackcollapse-perf.pl | ./FlameGraph/flamegraph.pl --color=java --hash > flame01.svg
+
+#.for pid in $*; do
+#  java -cp attach-main.jar:$JAVA_HOME/lib/tools.jar net.virtualvoid.perf.AttachOnce $pid
+#done
